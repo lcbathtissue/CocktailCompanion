bef65c1e87d95cf28014c24d3d5dd405fa92a560
Alden O'Cain <alden.ocain@ontariotechu.net>
2023-03-16 18:42:33 -0400

    MVP

--- CocktailCompanion.py
+++ CocktailCompanion.py
@@ -48,7 +48,17 @@ def search_ingredient_by_name(name):
     log_API_call(logger, "search_ingredient_by_name", name, data)
     return data
 
-def search_cocktail_by_ID(id):
+def search_ingredient_by_ID(id):
+    global logger
+    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php"
+    params = {"iid": id}
+    response = requests.get(url, params=params)
+    data = response.json()
+    save_JSON('search_ingredient_by_ID.json', data)
+    log_API_call(logger, "search_ingredient_by_ID", id, data)
+    return data
+
+def search_cocktail_by_ID(id): 
     global logger
     url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php"
     params = {"i": id}
@@ -88,12 +98,45 @@ def homepage():
             # print("\n\nNEW QUERY - TYPE 3\n\n")
             results = json_drink_ID(query)
 
-        titles = []
-        for drink in results['drinks']:
-            titles.append(drink['strDrink'])
+        if query_type == "cocktail_name" or query_type == "cocktail_id":
+            drinks = []
+            for drink in results['drinks']:
+                drinks.append(drink['strDrink'])
 
-        cards = [{"title": title, "description": "This is a card description."} for title in titles]
-        return render_template('index.html', cards=cards)
+            drink_IDs = []
+            for ID in results['drinks']:
+                drink_IDs.append(ID['idDrink'])
+
+            img_URLs = []
+            for URL in results['drinks']:
+                img_URLs.append(URL['strDrinkThumb'])
+
+            cards = [
+                {
+                    "title": title,
+                    "img_url": img_url,
+                    "id": drink_id
+                } for title, img_url, drink_id in zip(drinks, img_URLs, drink_IDs)
+            ]
+
+            return render_template('index.html', drink_cards=True, cards=cards)
+        elif query_type == "cocktail_ingredient":
+            ingredients = []
+            for ingredient in results['ingredients']:
+                ingredients.append(ingredient['strIngredient'])
+
+            ingredient_IDs = []
+            for ID in results['ingredients']:
+                ingredient_IDs.append(ID['idIngredient'])
+
+            cards = [
+                {
+                    "title": title,
+                    "id": ingredient_id
+                } for title, ingredient_id in zip(ingredients, ingredient_IDs)
+            ]
+
+            return render_template('index.html', drink_cards=False, cards=cards)
     return render_template('index.html')
 
 # PAGE RESPONSES
@@ -103,8 +146,13 @@ def show_drink(drink_ID):
     return render_template('drink.html', drink=drink_data['drinks'][0])
 
 @app.route('/ingredient_name/<ingredient_name>')
-def show_ingredient(ingredient_name):
+def show_ingredient_by_name(ingredient_name):
     ingredient_data = search_ingredient_by_name(ingredient_name)
+    return render_template('ingredient.html', ingredient_data=ingredient_data['ingredients'][0])
+
+@app.route('/ingredient_ID/<ingredient_ID>')
+def show_ingredient_by_ID(ingredient_ID):
+    ingredient_data = search_ingredient_by_ID(ingredient_ID)
     return render_template('ingredient.html', ingredient_data=ingredient_data['ingredients'][0])
 
 # JSON RESPONSES
@@ -112,11 +160,11 @@ def show_ingredient(ingredient_name):
 def json_all_drinks():
     return search_cocktail_by_name("")
 
-@app.route('/json/drink_name/<name>')
+@app.route('/json/drink_name/<drink_name>')
 def json_drink_search(drink_name):
     return search_cocktail_by_name(drink_name)
 
-@app.route('/json/ingredient_name/<name>')
+@app.route('/json/ingredient_name/<ingredient_name>')
 def json_ingredient_search(ingredient_name):
     return search_ingredient_by_name(ingredient_name)
 
